7c486a82f63f42ffbd748eb8cee6da6ef7082b9b
nmukiti <nmukiti@kabarak.ac.ke>
2021-06-09 12:32:44 +0300

    fisrt commit

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView} from 'react-native';
+
+import Header from './src/components/Header';
 
 export default function App() {
+
+  const [todoItem, setTodoItem] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodoList = () => { 
+   setTodoList([...todoList, todoItem])};
+   console.log(todoList);
   return (
+    <View>
+      <Header title="Todo List"/>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <View>
+       <TextInput placeholder="Enter ToDo Item"
+         style={styles.textInput}
+         onChangeText={text => setTodoItem(text)}
+         value={todoItem}></TextInput>
+       <Button
+        title="Add Todo"
+        onPress ={addTodoList}
+        />
+     </View>
+     <ScrollView>
+       {todoList.map(todo => <View key={todo} style={styles.todoItem}>
+         <Text>{todo}</Text>
+         </View>)}
+     </ScrollView>
+    </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   padding: 30
   },
+  textInput: {
+    padding: 10,
+    borderColor: '#000000',
+    marginBottom: 10,
+    borderWidth: 1
+  },
+  todoItem: {
+    marginTop: 10,
+    padding: 20,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'gray'
+  }
 });
